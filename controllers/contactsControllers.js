16932971de19293a49e7e16492dfe67dd1723d26
Ilya Bondarchuk <cod3provider@gmail.com>
2024-05-08 10:19:49 +0200

    added all services, some controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,61 @@
-import contactsService from "../services/contactsServices.js";
+// import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+import {addContact, getContactById, listContacts} from "../services/contactsServices.js";
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await listContacts();
+    res.json(contacts);
+  }
+  catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const contactById = await getContactById(id);
+    if (!contactById) {
+      return res.status(404).json({
+        message: `Contact with id ${id} not found`
+      })
+    }
+    res.json(contactById);
+  }
+  catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  try {
+    console.log(req.body)
+    const result = await addContact(req.body);
+    console.log(result)
+    res.status(201).json(result);
+  }
+  catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+};
+
+export const updateContact = async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+};
